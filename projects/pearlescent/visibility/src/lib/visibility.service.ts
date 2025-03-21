cda448cd870a2438ea19a3b4c982aed7ac58fdb3
Ezra Carter <31769576+themanciraptor@users.noreply.github.com>
2025-03-21 16:11:44 -0600

    visibility (bugfix): subsequent instances weren't being observed

--- projects/pearlescent/visibility/src/lib/visibility.service.ts
+++ projects/pearlescent/visibility/src/lib/visibility.service.ts
@@ -1,47 +1,50 @@
-import { afterNextRender, DestroyRef, Injectable, WritableSignal } from '@angular/core';
+import { afterNextRender, afterRenderEffect, DestroyRef, Injectable, signal, WritableSignal } from '@angular/core';
 
 @Injectable({
   providedIn: 'root',
 })
 export class VisibilityService {
-  private readonly registeredEls = new Map<Element, WritableSignal<boolean>>();
+  private readonly registeredEls = signal<Map<Element, WritableSignal<boolean>>>(new Map());
   private intersectionObserver?: IntersectionObserver;
 
   constructor() {
-    afterNextRender(() => this.initializeIntersectionObserver());
+    afterNextRender({
+      read: () => this.initializeIntersectionObserver(),
+    });
+    afterRenderEffect({
+      read: () => {
+        const m = this.registeredEls();
+        m.forEach((_, k) => this.intersectionObserver?.observe(k));
+      },
+    });
   }
 
   private initializeIntersectionObserver() {
     this.intersectionObserver = new IntersectionObserver(
       (entries) => {
         entries.forEach((entry) => {
-          const isVisible = this.registeredEls.get(entry.target);
+          const isVisible = this.registeredEls().get(entry.target);
           if (!isVisible) return this.unregister(entry.target);
-          if (entry.isIntersecting) {
-            isVisible?.set(true);
-          } else {
-            isVisible?.set(false);
-          }
+          isVisible.set(entry.isIntersecting);
         });
       },
       { threshold: [0.0, 1.0] }
     );
-
-    this.registeredEls.forEach((_, el) => this.intersectionObserver?.observe(el));
   }
 
   public register(el: Element, signalRef: WritableSignal<boolean>, destroyRef: DestroyRef) {
-    this.registeredEls.set(el, signalRef);
-
+    this.registeredEls.set(this.registeredEls().set(el, signalRef));
     destroyRef.onDestroy(() => {
       this.unregister(el);
     });
   }
 
   public unregister(el: Element) {
-    this.registeredEls.delete(el);
+    const m = this.registeredEls();
+    m.delete(el);
+    this.registeredEls.set(m);
     this.intersectionObserver?.unobserve(el);
-    if (this.registeredEls.size === 0) {
+    if (this.registeredEls().size === 0) {
       this.intersectionObserver?.disconnect();
     }
   }
