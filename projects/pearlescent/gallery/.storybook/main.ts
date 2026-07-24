25b06d3589279c010c8ea09d8cc7bdf9bc6d45fa
Ezra Carter <31769576+themanciraptor@users.noreply.github.com>
2026-07-24 15:18:38 -0600

    upgrade story books versions, adjust npm deps for packages

--- projects/pearlescent/gallery/.storybook/main.ts
+++ projects/pearlescent/gallery/.storybook/main.ts
@@ -3,10 +3,9 @@ import type { StorybookConfig } from '@storybook/angular';
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: [
-    '@storybook/addon-essentials',
     '@storybook/addon-onboarding',
     '@chromatic-com/storybook',
-    '@storybook/addon-interactions',
+    '@storybook/addon-docs'
   ],
   framework: {
     name: '@storybook/angular',
